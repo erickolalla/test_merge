d4729f2863335b8c425ef051d8a024f2f94b9eae
erickolalla <erickfabri.01@gmail.com>
2025-11-11 00:10:57 -0500

    Modificación al archivo del Barrolo

--- file.py
+++ file.py
@@ -6,3 +6,10 @@ for i in range(10):
 print("")
 for j in range(10):
     print(f"Hola Bartolitooooo {j+1}")
+
+
+print("")
+for k in range(1, 5+1):
+    espacios = " " * (5 - k)
+    print(espacios + "*" * (2 * k - 1))
+    
