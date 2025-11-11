153ddea84ed8b5e15b4eb78a07603784c7c1c53e
erickolalla <erickfabri.01@gmail.com>
2025-11-10 23:52:41 -0500

    Mensaje para el Bartolito

---
+++ file2.py
@@ -0,0 +1,9 @@
+print ("Todo lo que diga la jefa Bartomé" \
+       " es la pura verdad")
+
+for i in range(100):
+    if i % 2 == 0:
+        print("Le caigo mal a la jefa Bartomé :3")
+        print("\n")
+    else:
+        print("Le caigo bien a la jefa Bartomé :( ")
